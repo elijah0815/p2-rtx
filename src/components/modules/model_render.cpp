bced6e2fd604a735f843f2d5e2ff9fc512968969
#xoxor4d <r4d0xzz@live.de>
2024-10-13 01:54:25 +0200

    fix some effects (NO FF ..)

--- src/components/modules/model_render.cpp
+++ src/components/modules/model_render.cpp
@@ -164,7 +164,7 @@ namespace components
 	}
 
 	// can be used to figure out the layout of the vertex buffer
-	void lookat_vertex_decl([[maybe_unused]] IDirect3DDevice9* dev)
+	void lookat_vertex_decl([[maybe_unused]] IDirect3DDevice9* dev, CPrimList* primlist = nullptr)
 	{
 #ifdef DEBUG
 		IDirect3DVertexDeclaration9* vertex_decl = nullptr;
@@ -227,6 +227,80 @@ namespace components
 		d3dvertelem decl[MAX_FVF_DECL_SIZE]; UINT numElements = 0;
 		vertex_decl->GetDeclaration((D3DVERTEXELEMENT9*)decl, &numElements);
 		int break_me = 1; // look into decl
+
+		if (primlist)
+		{
+			IDirect3DVertexBuffer9* vb = nullptr; UINT t_stride = 0u, t_offset = 0u;
+			dev->GetStreamSource(0, &vb, &t_offset, &t_stride);
+
+			//auto first_vert = *reinterpret_cast<std::uint32_t*>(RENDERER_BASE + 0x17547C);
+			//auto num_verts_real = *reinterpret_cast<std::uint32_t*>(RENDERER_BASE + 0x1754A0);
+
+			IDirect3DIndexBuffer9* ib = nullptr;
+			if (SUCCEEDED(dev->GetIndices(&ib)))
+			{
+				void* ib_data; // lock index buffer to retrieve the relevant vertex indices
+				if (SUCCEEDED(ib->Lock(0, 0, &ib_data, D3DLOCK_READONLY)))
+				{
+					// add relevant indices without duplicates
+					std::unordered_set<std::uint16_t> indices; indices.reserve(primlist->m_NumIndices);
+					for (auto i = 0u; i < (std::uint32_t)primlist->m_NumIndices; i++)
+					{
+						indices.insert(static_cast<std::uint16_t*>(ib_data)[primlist->m_FirstIndex + i]);
+					}
+
+					ib->Unlock();
+
+					// get the range of vertices that we are going to work with
+					UINT min_vert = 0u, max_vert = 0u;
+					{
+						auto [min_it, max_it] = std::minmax_element(indices.begin(), indices.end());
+						min_vert = *min_it;
+						max_vert = *max_it;
+					}
+
+					void* src_buffer_data;
+
+					// lock vertex buffer from first used vertex (in total bytes) to X used vertices (in total bytes)
+					if (SUCCEEDED(vb->Lock(min_vert * t_stride, max_vert * t_stride, &src_buffer_data, 0)))
+					{
+						struct src_vert
+						{
+							Vector pos;				 
+							D3DCOLOR color;
+							Vector4D tc0;		 
+							Vector4D tc1;		 
+							Vector4D tc2; 
+							Vector2D tc3;
+							Vector4D tc4;			 
+							//Vector4D tc5;			 
+							//Vector4D tc6;
+							//Vector4D tc7;
+						};
+
+						for (auto i : indices)
+						{
+							// we need to subtract min_vert because we locked @ min_vert which is the start of our lock
+							i -= min_vert;
+
+							const auto v_pos_in_src_buffer = i * t_stride;
+							const auto src = reinterpret_cast<src_vert*>(((DWORD)src_buffer_data + v_pos_in_src_buffer));
+
+							int break_me = 1;
+
+							//if (src->tc7.z != 1.337f)
+							{
+								//src->tc7.z = 1.337f;
+								src->tc0.x = std::lerp(src->tc0.z, src->tc0.x, src->tc3.x);
+								src->tc0.y = std::lerp(src->tc0.w, src->tc0.y, src->tc3.y);
+							}
+						}
+
+						vb->Unlock();
+					}
+				}
+			}
+		}
 #endif
 	}
 
@@ -1132,25 +1206,8 @@ namespace components
 				//ctx.modifiers.do_not_render = true;
 
 				dev->SetTransform(D3DTS_WORLD, &ctx.info.buffer_state.m_Transform[0]);
-				dev->SetTransform(D3DTS_VIEW, &ctx.info.buffer_state.m_Transform[1]);
+				dev->SetTransform(D3DTS_VIEW, &ctx.info.buffer_state.m_Transform[1]); 
 				dev->SetTransform(D3DTS_PROJECTION, &ctx.info.buffer_state.m_Transform[2]);
-
-#if 0			// can be used to look into the vertex buffer to figure out the layout
-				{
-					IDirect3DVertexBuffer9* buff = nullptr;
-					UINT t_stride = 0u, t_offset = 0u;
-					dev->GetStreamSource(0, &buff, &t_offset, &t_stride);
-
-					void* buffer_data;
-					if (buff)
-					{
-						if (const auto hr = buff->Lock(0, 48u * 100u, &buffer_data, D3DLOCK_READONLY); hr >= 0)
-						{
-							buff->Unlock(); // break here
-						}
-					}
-				}
-#endif
 			}
 
 			// general models (eg. furniture in first lvl - container)
@@ -1230,25 +1287,7 @@ namespace components
 #ifdef DEBUG
 				//ctx.modifiers.do_not_render = true;
 				int break_me = 0;
-#if 0			// can be used to look into the vertex buffer to figure out the layout
-				{
-					IDirect3DVertexBuffer9* buff = nullptr;
-					UINT t_stride = 0u, t_offset = 0u;
-					dev->GetStreamSource(0, &buff, &t_offset, &t_stride);
 
-					void* buffer_data;
-					if (buff)
-					{
-						if (const auto hr = buff->Lock(0, t_stride * 2, &buffer_data, D3DLOCK_READONLY); hr >= 0)
-						{
-							buff->Unlock(); // break here
-						}
-					}
-
-					//dev->SetStreamSource(0, buff, t_offset, 48*4); // times 48*4 is almost good lmao
-					//render_with_new_stride = true;
-				}
-#endif
 				//dev->SetTexture(0, tex_addons::portal_mask);
 
 				//ctx.save_vs(dev);
@@ -1259,6 +1298,16 @@ namespace components
 				//dev->SetTransform(D3DTS_VIEW, &buffer_state.m_Transform[1]);
 				//dev->SetTransform(D3DTS_PROJECTION, &buffer_state.m_Transform[2]);
 #endif
+
+				lookat_vertex_decl(dev, primlist);
+
+				//ctx.save_vs(dev);
+				//dev->SetVertexShader(nullptr);
+				//dev->SetFVF(D3DFVF_XYZ | D3DFVF_DIFFUSE | D3DFVF_TEX6);
+
+				//dev->SetTransform(D3DTS_WORLD, &ctx.info.buffer_state.m_Transform[0]);  
+				//dev->SetTransform(D3DTS_VIEW, &ctx.info.buffer_state.m_Transform[1]);
+				//dev->SetTransform(D3DTS_PROJECTION, &ctx.info.buffer_state.m_Transform[2]);
 			}
 
 			// on portal open - blob in the middle (impact)
@@ -1343,6 +1392,73 @@ namespace components
 			else if (mesh->m_VertexFormat == 0x24914900005) // stride 144 ....
 			{
 				//ctx.modifiers.do_not_render = true;
+
+				
+
+				// a little heavy but this fixes shader rendering
+				// FF wont draw anything ... ?
+
+				IDirect3DVertexBuffer9* vb = nullptr; UINT t_stride = 0u, t_offset = 0u;
+				if (SUCCEEDED(dev->GetStreamSource(0, &vb, &t_offset, &t_stride)))
+				{
+					IDirect3DIndexBuffer9* ib = nullptr;
+					if (SUCCEEDED(dev->GetIndices(&ib)))
+					{
+						void* ib_data; // lock index buffer to retrieve the relevant vertex indices
+						if (SUCCEEDED(ib->Lock(0, 0, &ib_data, D3DLOCK_READONLY)))
+						{
+							// add relevant indices without duplicates
+							std::unordered_set<std::uint16_t> indices; indices.reserve(primlist->m_NumIndices);
+							for (auto i = 0u; i < (std::uint32_t)primlist->m_NumIndices; i++)
+							{
+								indices.insert(static_cast<std::uint16_t*>(ib_data)[primlist->m_FirstIndex + i]);
+							}
+							ib->Unlock();
+
+							// get the range of vertices that we are going to work with
+							UINT min_vert = 0u, max_vert = 0u;
+							{
+								auto [min_it, max_it] = std::minmax_element(indices.begin(), indices.end());
+								min_vert = *min_it; max_vert = *max_it;
+							}
+
+							void* src_buffer_data;
+
+							// lock vertex buffer from first used vertex (in total bytes) to X used vertices (in total bytes)
+							if (SUCCEEDED(vb->Lock(min_vert * t_stride, max_vert * t_stride, &src_buffer_data, 0)))
+							{
+								struct src_vert
+								{
+									Vector pos; D3DCOLOR color;
+									Vector4D tc0; Vector4D tc1; Vector4D tc2; Vector2D tc3;
+									Vector4D tc4; Vector4D tc5; Vector4D tc6; Vector4D tc7;
+								};
+
+								for (auto i : indices)
+								{
+									// we need to subtract min_vert because we locked @ min_vert which is the start of our lock
+									i -= min_vert;
+
+									const auto v_pos_in_src_buffer = i * t_stride;
+									const auto src = reinterpret_cast<src_vert*>(((DWORD)src_buffer_data + v_pos_in_src_buffer));
+
+									src->tc0.x = std::lerp(src->tc0.z, src->tc0.x, src->tc3.x);
+									src->tc0.y = std::lerp(src->tc0.w, src->tc0.y, src->tc3.y);
+
+									// float2 vCornerID : TEXCOORD3;
+									// o.texCoord0_1.xy = lerp( v.vTexCoord0.zw, v.vTexCoord0.xy, v.vCornerID.xy );
+								}
+
+								vb->Unlock();
+							}
+						}
+					}
+				}
+
+				//ctx.save_vs(dev);
+				//dev->SetVertexShader(nullptr);
+				//dev->SetFVF(D3DFVF_XYZ | D3DFVF_DIFFUSE | D3DFVF_TEX8);
+				//dev->SetTransform(D3DTS_WORLD, &ctx.info.buffer_state.m_Transform[0]);
 			}
 
 			// paint blobs (blob only, not the painted surfs)
